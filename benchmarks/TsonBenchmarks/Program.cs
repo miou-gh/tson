6d7d5d62f0dd0d18941dc93bf9aa065ef9beb14b
atillabyte <atillabyte@null.net>
2020-08-05 07:05:00 -0400

    v3.2.2 - update benchmarks

--- benchmarks/TsonBenchmarks/Program.cs
+++ benchmarks/TsonBenchmarks/Program.cs
@@ -57,21 +57,43 @@ namespace TsonBenchmarks
     {
         private string json_string;
         private string tson_string;
+        private Temperatures object_to_serialize;
+        private readonly int iteration_count = 50;
 
         [GlobalSetup]
         public void Setup()
         {
             json_string = "{ \"description\": { \"title\": \"Contiguous U.S., Average Temperature\", \"units\": \"Degrees Fahrenheit\", \"base_period\": \"1901-2000\" }, \"data\": { \"189512\": { \"value\": \"50.34\", \"anomaly\": \"-1.68\" }, \"189612\": { \"value\": \"51.99\", \"anomaly\": \"-0.03\" }, \"189712\": { \"value\": \"51.56\", \"anomaly\": \"-0.46\" } } }";
             tson_string = "{\"description\":{\"title\":string(\"Contiguous U.S., Average Temperature\"),\"units\":string(\"Degrees Fahrenheit\"),\"base_period\":string(\"1901-2000\")},\"data\":{\"189512\":{\"value\":string(\"50.34\"),\"anomaly\":string(\"-1.68\")},\"189612\":{\"value\":string(\"51.99\"),\"anomaly\":string(\"-0.03\")},\"189712\":{\"value\":string(\"51.56\"),\"anomaly\":string(\"-0.46\")}}}";
-
-            DeserializeFromJSON();
-            DeserializeFromTSON();
+            object_to_serialize = JsonConvert.DeserializeObject<Temperatures>(json_string);
         }
 
         [Benchmark]
-        public void DeserializeFromJSON() => JsonConvert.DeserializeObject<Temperatures>(json_string);
+        public void DeserializeFromJSON()
+        {
+            for (var i = 0; i < iteration_count; i++)
+                JsonConvert.DeserializeObject<Temperatures>(json_string);
+        }
 
         [Benchmark]
-        public void DeserializeFromTSON() => TsonConvert.DeserializeObject<Temperatures>(tson_string);
+        public void DeserializeFromTSON()
+        {
+            for (var i = 0; i < iteration_count; i++)
+                TsonConvert.DeserializeObject<Temperatures>(tson_string);
+        }
+
+        [Benchmark]
+        public void SerializeToJSON()
+        {
+            for (var i = 0; i < iteration_count; i++)
+                JsonConvert.SerializeObject(object_to_serialize, Newtonsoft.Json.Formatting.None);
+        }
+
+        [Benchmark]
+        public void SerializeToTSON()
+        {
+            for (var i = 0; i < iteration_count; i++)
+                TsonConvert.SerializeObject(object_to_serialize, Tson.Formatting.None);
+        }
     }
 }
